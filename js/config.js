a48b7a79a70022bd165a0255a12cf9ab8c5a059a
sanjay-jr <60375824+sanjay-jr@users.noreply.github.com>
2023-12-05 22:59:06 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-2_8HcxTj3V3',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '29003d9ib28qm1i1mak7fncp0n',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
