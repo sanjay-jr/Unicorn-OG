783b22395247a612fccbded9d52d434d67a8c84c
sanjay-jr <60375824+sanjay-jr@users.noreply.github.com>
2023-12-08 20:13:41 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-2_8HcxTj3V3',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '29003d9ib28qm1i1mak7fncp0n',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId:         'us-east-2_J1EZDGz83',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '5kb1qv3ktojldtbfjrntr3upj6',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
